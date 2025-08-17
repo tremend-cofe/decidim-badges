05899d98a222a10cf181a4d401ba30684195ec70
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-08-17 17:44:17 +0300

    Add possibility of editing the badge

--- app/models/decidim/badges/badge.rb
+++ app/models/decidim/badges/badge.rb
@@ -2,15 +2,27 @@
 module Decidim
   module Badges
     class Badge < ApplicationRecord
+      include Decidim::HasAttachments
       include Decidim::HasUploadValidations
+      include Decidim::Loggable
       include Decidim::Publicable
 
       belongs_to :gamifiable, polymorphic: true
 
       has_one_attached :file
-      # validates_upload :file, uploader: Decidim::ImageUploader
+      # validates :file, presence: true, unless: :persisted?
+
+      validates_upload :file, uploader: Decidim::ImageUploader
 
       default_scope -> { order(:weight) }
+
+      def self.log_presenter_class_for(_log)
+        Decidim::Badges::AdminLog::BadgePresenter
+      end
+
+      def organization
+        gamifiable.is_a?(Decidim::Organization) ? gamifiable : gamifiable.try(:organization)
+      end
     end
   end
 end
