d4baee13608575485a82f09d580f732ff14484fb
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-08-18 01:49:01 +0300

    Add Participatory Space constraint interface

--- app/commands/decidim/badges/admin/create_badge.rb
+++ app/commands/decidim/badges/admin/create_badge.rb
@@ -4,7 +4,8 @@ module Decidim
   module Badges
     module Admin
       class CreateBadge < Decidim::Commands::CreateResource
-        fetch_form_attributes :name, :organization, :earning_methods, :description, :levels
+        fetch_form_attributes :name, :organization, :earning_methods, :description, :levels,
+                              :participatory_space_type, :participatory_space_id, :component
         fetch_file_attributes :file
 
         def resource_class = Decidim::Badges::Badge
