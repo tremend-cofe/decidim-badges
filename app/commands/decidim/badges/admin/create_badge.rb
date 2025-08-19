1ca7403ec70e6621c58d75b4681925fd331043e6
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-08-19 11:55:03 +0300

    Add small fixes

--- app/commands/decidim/badges/admin/create_badge.rb
+++ app/commands/decidim/badges/admin/create_badge.rb
@@ -5,7 +5,7 @@ module Decidim
     module Admin
       class CreateBadge < Decidim::Commands::CreateResource
         fetch_form_attributes :name, :organization, :earning_methods, :description, :levels,
-                              :participatory_space_type, :participatory_space_id, :decidim_component_id
+                              :participatory_space_type, :participatory_space_id, :decidim_component_id, :action_name
         fetch_file_attributes :file
 
         def resource_class = Decidim::Badges::Badge
