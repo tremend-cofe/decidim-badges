8b761593b147ce9e1b05b4c8a2d899428d290658
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-08-20 02:09:37 +0300

    Add Badge page

--- lib/decidim/badges/engine.rb
+++ lib/decidim/badges/engine.rb
@@ -2,6 +2,7 @@
 
 require "rails"
 require "decidim/core"
+require "deface"
 
 module Decidim
   module Badges
@@ -12,6 +13,19 @@ module Decidim
       initializer "decidim_badges.add_cells_view_paths", before: "decidim_core.add_cells_view_paths"  do
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Badges::Engine.root}/app/cells")
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Badges::Engine.root}/app/views")
+      end
+
+      initializer "decidim_badges.deface" do
+        Rails.application.configure do
+          config.deface.enabled = Decidim::Env.new("DEFACE_ENABLED", true).present?
+        end
+      end
+
+      initializer "decidim_badges.add_badges" do
+        Rails.application.config.to_prepare do
+          Decidim::Gamification::BadgesController.prepend(Decidim::Badges::Overwrites::BadgesController)
+          Decidim::Gamification::BadgesController.helper(Decidim::ResourceHelper)
+        end
       end
 
     end
