b026abfcd18433b570cc0eb96c930d75892d7819
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-11 23:48:02 +0200

    Add Create Badge functionality (#4)

--- app/commands/decidim/badges/admin/create_badge.rb
+++ app/commands/decidim/badges/admin/create_badge.rb
@@ -3,9 +3,11 @@
 module Decidim
   module Badges
     module Admin
-      class UpdateBadge < Decidim::Commands::UpdateResource
+      class CreateBadge < Decidim::Commands::CreateResource
         fetch_form_attributes :name, :gamifiable, :earning_methods, :description, :levels
         fetch_file_attributes :file
+
+        def resource_class = Decidim::Badges::Badge
       end
     end
   end
