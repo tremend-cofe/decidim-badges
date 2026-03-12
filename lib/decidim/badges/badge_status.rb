52081e793afde3a127c33c20cf041d4461fe3f1e
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-03-12 15:20:49 +0200

    [CITI-995] Apply review recommendations

--- lib/decidim/badges/badge_status.rb
+++ lib/decidim/badges/badge_status.rb
@@ -25,7 +25,11 @@ module Decidim
       def next_level_in
         return nil if last_level?
 
-        @badge.levels[level.to_s].try(:to_i) - score
+        threshold = @badge.levels[level.to_s]
+
+        return nil if threshold.blank?
+
+        threshold.to_i - score
       end
 
       # Public: Returns the score of a user on the badge.
@@ -36,6 +40,8 @@ module Decidim
       end
 
       def level_up
+        return 1.0 if last_level?
+
         score.to_f / (score + next_level_in.to_f)
       end
 
